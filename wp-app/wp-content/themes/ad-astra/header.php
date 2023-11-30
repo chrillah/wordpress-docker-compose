d67029041c973a64d89eb89783eb551fcb85ccfb
chrillah <ch.ronnberg@gmail.com>
2023-11-30 09:55:06 +0100

    update

--- wp-app/wp-content/themes/ad-astra/header.php
+++ wp-app/wp-content/themes/ad-astra/header.php
@@ -10,25 +10,17 @@
 <body>
     <div id="wrap">
         <header id="header">
-        <div class="header-container">
-                    <div>
-                        <a class="logo-type" href="index.html">Labb 1</a>
-                    </div>
-                    <form
-                        id="searchform"
-                        class="searchform"
-                        action="search.php"
-                    >
-                        <div>
-                            <input placeholder="Sök efter..." type="text" />
-                            <input
-                                class="app-button"
-                                type="submit"
-                                value="Sök"
-                            />
-                        </div>
-                    </form>
+            <div class="header-container">
+                <div>
+                    <a class="logo-type" href="index.html">Labb 1</a>
                 </div>
+                <form id="searchform" class="searchform" action="search.php">
+                    <div>
+                        <input placeholder="Sök efter..." type="text" value="<?php get_search_query(); ?>" name="s" />
+                        <input class="app-button" type="submit" value="Sök" />
+                    </div>
+                </form>
+            </div>
 
             <!-- <nav id="nav">
                 <div class="nav-container">
