74aef485b6ef2084dc3c3edf96f5f73a8c83289a
chrillah <ch.ronnberg@gmail.com>
2023-12-04 16:00:25 +0100

    update

---
+++ wp-app/wp-content/themes/ad-astra/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<main class="a-page">
+    <section class="hero">
+        <div class="hero-image">
+            <div class="hero-text-container">
+                <h1 class="hero-title">404</h1>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
